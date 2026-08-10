5ad5f2ec6306670971d2d895e357f56659c6426a
Dũng Bùi <buihoangducdung04@gmailcom>
2026-08-10 15:42:59 +0700

    Fix bug liveness

--- src/components/LecturerLivenessLogin.tsx
+++ src/components/LecturerLivenessLogin.tsx
@@ -158,7 +158,6 @@ export function LecturerLivenessLogin({
         .amplify-liveness-camera-module {
           height: 100% !important;
           width: 100% !important;
-          background-color: transparent !important;
           display: flex !important;
           align-items: center !important;
           justify-content: center !important;
